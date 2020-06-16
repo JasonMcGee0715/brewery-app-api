43fca988cce528654e1ac3a9d8fb352489275c99
JasonMcGee0715 <jdm0715@gmail.com>
2020-06-15 21:23:18 -0500

    reaching api done

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-require('dotenv').config()
+// require('dotenv').config()
+
+window.onload = () => {
+    getLocation();
+}
+
+const getLocation = () => {
+    fetch('https://api.brewerydb.com/v2/brewery/random/?key=456c069a108bcae9db40e3e0dac4b87c')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
+
